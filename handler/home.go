68227ce50de4d308292827691cabd0655bb6dd52
Yann Landry <landryy@twitch.tv>
2022-01-26 02:58:16 -0800

    Remove window title

--- handler/home.go
+++ handler/home.go
@@ -11,6 +11,5 @@ type HomePresenter struct {
 
 func Home(response http.ResponseWriter, request *http.Request) {
 	presenter := NewBasePresenter(&HomePresenter{})
-	presenter.WindowTitle = "yannlandry.photography"
 	ExecuteTemplate(content.Content.Home.Template, response, presenter)
 }
